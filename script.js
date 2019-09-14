960c78d28a60ac1119e2a2285996daac425bf2ec
Kristen <hackettka@gmail.com>
2019-09-14 08:31:16 -0400

    Tip calculator program

--- script.js
+++ script.js
@@ -1,30 +1,13 @@
-// for (let i = 1; i < 4; i = i + 1 ) {
-//   $("#response").append("<br />" + i);
-// }
-//
-// Write a program so that when you reload your page, it asks for a number and prints all the numbers from 1 to it, including it;
+let tipCalculator;
+tipCalculator = function(total, tipRate){
+  // step 1:
+  let tipAmount;
+  tipAmount = tipRate * total;
+  // and step 2:
+  $("#response").html("Your tip is $" + tipAmount);
+};
 
+// Now call (or “execute”) the function, passing a
+// total of $50.00 and a tipRate of 20%:
 
-let userInput;
-userInput = prompt("What's your favorite number between 1 and 5", "Type your answer here.");
-let i;
-i = 1;
-while ( i => userInput ) {
-  $("#response").html("<br />" + i);
-  i = i + 1;
-}
-
-// if ( userInput === "burrito" ) {
-//   $("#response").html("Brilliant choice!");
-// }
-// else {
-//   $("#response").html("Don’t you want a burrito?");
-// }
-
-
-// let i;
-// i = 1;
-// while ( i < 4 ) {
-//   $("#response").append("<br />" + i);
-//   i = i + 1;
-// }
+tipCalculator(50.00, 0.2);
